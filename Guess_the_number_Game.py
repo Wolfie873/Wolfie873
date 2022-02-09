8d9b393e7ef3fcc1e3224e122ad2cfde36f8e58c
Wolfie873 <jorge.ortiz20@outlook.com>
2022-02-09 19:04:56 -0400

    Create Guess_the_number_Game.py

---
+++ Guess_the_number_Game.py
@@ -0,0 +1,22 @@
+#Guess the number Game
+import random #imports random library
+
+print("Hello, what is your Name: ") #print writes stuff
+name = input() #variables are created without value, no int var necessary
+
+print("Well " + name + ", I'm thinking of a number between 1 and 20")
+secretNumber = random.randint(1, 20)#This saves a random integer between 1 and 20
+
+for guessesTaken in range(1, 7): #notice built in variable range
+    print("Take a guess: ")
+    guess = int(input()) #Converts string into integer
+
+    if guess < secretNumber:
+        print("Nope, too low")
+    elif guess > secretNumber:
+        print("Nope, too high")
+    elif guess == secretNumber:
+        print("Ding, ding, ding, ding, ding, ding")
+        break
+
+print("You took " + str(guessesTaken) + " guesses.")
