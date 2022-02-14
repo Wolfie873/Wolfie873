2ac9d37a978b1053fc0bdad8102a2ed0e0ffa5fe
Wolfie873 <50751189+Wolfie873@users.noreply.github.com>
2022-02-14 14:04:05 -0500

    Src change for snow.gif

--- Webpage Project/Snow.js
+++ Webpage Project/Snow.js
@@ -1,11 +1,11 @@
-let x = document.getElementById('hideimage');
-function snowing(){
-	document.body.style.background = "url('Pixelart/snow.gif') #ACBEBE";
-    x.style.display = "run-in";
-	}
-			
-function stop_snowing(){
-	document.body.style.background = "#F4F4EF";
-    x.style.display = "run-in";
-	x.style.animation = "none";
-	}
+let x = document.getElementById("hideimage");
+function snowing() {
+  document.body.style.background = "url('images/pixelart/snow.gif') #ACBEBE";
+  x.style.display = "run-in";
+}
+
+function stop_snowing() {
+  document.body.style.background = "#F4F4EF";
+  x.style.display = "run-in";
+  x.style.animation = "none";
+}
