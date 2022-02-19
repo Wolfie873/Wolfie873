8191dfa954a852bf006e1879fcfa260143dd0568
Wolfie873 <jorge.ortiz20@outlook.com>
2022-02-19 13:23:46 -0400

    charcount program

--- Python Practice/characterCount.py
+++ Python Practice/characterCount.py
@@ -1,9 +1,11 @@
-message = 'It was a cold day in June and before I continue let me stop.'
+import pprint
+
+message = 'It was a cold day in June and before I continue let me stop.' # The triple quote ''' can be used for inserting multiple lines of text
 msg = input()
 count = {} # Something like 'r' is the key and it counts how many r there are
 
-for character in msg:
+for character in msg.upper():
     count.setdefault(character, 0)
     count[character] = count[character] + 1
     
-print(count)
+pprint.pprint(count)
