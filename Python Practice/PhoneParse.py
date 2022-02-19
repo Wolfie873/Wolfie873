ae022616688c55dac7387ccde86db35ab59d1c07
Wolfie873 <jorge.ortiz20@outlook.com>
2022-02-19 19:42:22 -0400

    learning parse

---
+++ Python Practice/PhoneParse.py
@@ -0,0 +1,28 @@
+#! python3
+def phoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+message = input('Please enter long text:')
+foundNumber = False
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if phoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+        foundNumber = True
+    if not foundNumber:
+        print('Phone number not found.')
---
+++ Python Practice/PhoneParsereg.py
@@ -0,0 +1,11 @@
+#! python3
+from ctypes.wintypes import HDC
+import re
+
+teststring = 'This is a text string with two phone numbers and two other things '\
+    '234-837-2883 and 455-382-8574 and 334432-35533-2 theres also 4334-32-3455'
+#message = input('Please enter your text here: ')
+
+phoneNumRegEx = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegEx.findall(teststring)
+print(str(mo))
