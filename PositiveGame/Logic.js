140e07dbfeee8562bea3e91a54b58b3b2dc7e3b4
Wolfie873 <jorge.ortiz20@outlook.com>
2022-03-05 20:31:18 -0400

    commit

---
+++ PositiveGame/Logic.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  document.getElementById("minushp").onclick = function () {
+    var hp = 0;
+    var check = false;
+
+    hp = hp - 1;
+    document.getElementById("hp").innerHTML = String(hp);
+  };
+  document.getElementById("plushp").onclick = function () {};
+  document.getElementById("minusmp").onclick = function () {};
+  document.getElementById("plusmp").onclick = function () {};
+  document.getElementById("minusstr").onclick = function () {};
+  document.getElementById("plusstr").onclick = function () {};
+  document.getElementById("minuslck").onclick = function () {};
+  document.getElementById("pluslck").onclick = function () {};
+};
