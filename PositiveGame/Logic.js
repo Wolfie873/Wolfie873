feda9dc8b76c4f5c336536ae10411c3f3763f61b
Wolfie873 <jorge.ortiz20@outlook.com>
2022-03-06 08:30:38 -0400

    comit

--- PositiveGame/Logic.js
+++ PositiveGame/Logic.js
@@ -1,16 +1,19 @@
 window.onload = function () {
-  document.getElementById("minushp").onclick = function () {
-    var hp = 0;
-    var check = false;
+  var xpPoints = document.getElementById("xp").innerHTML;
+  var x = 5;
 
-    hp = hp - 1;
-    document.getElementById("hp").innerHTML = String(hp);
-  };
-  document.getElementById("plushp").onclick = function () {};
-  document.getElementById("minusmp").onclick = function () {};
-  document.getElementById("plusmp").onclick = function () {};
-  document.getElementById("minusstr").onclick = function () {};
-  document.getElementById("plusstr").onclick = function () {};
-  document.getElementById("minuslck").onclick = function () {};
-  document.getElementById("pluslck").onclick = function () {};
+  for (var i = 0; i < x; i++) {
+    document.getElementById("minushp").onclick = function () {
+      var hp = 0;
+      hp = hp - 1;
+      document.getElementById("hp").innerHTML = String(hp);
+    };
+    document.getElementById("plushp").onclick = function () {};
+    document.getElementById("minusmp").onclick = function () {};
+    document.getElementById("plusmp").onclick = function () {};
+    document.getElementById("minusstr").onclick = function () {};
+    document.getElementById("plusstr").onclick = function () {};
+    document.getElementById("minuslck").onclick = function () {};
+    document.getElementById("pluslck").onclick = function () {};
+  }
 };
