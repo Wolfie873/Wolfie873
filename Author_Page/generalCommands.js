fa6cb20b9796adefbd5deb48ec02a66caea9479a
Wolfie873 <jorge.ortiz20@outlook.com>
2022-03-06 13:20:28 -0400

    added stuff

---
+++ Author_Page/generalCommands.js
@@ -0,0 +1,21 @@
+window.onload = function () {};
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+function scrolling() {
+  document.getElementById("div_top").scrollIntoView();
+}
+function viewer() {
+  if (document.getElementById("documentViewer").style.display == "none") {
+    document.getElementById("documentViewer").style.display = "block";
+  } else {
+    document.getElementById("documentViewer").style.display = "none";
+  }
+}
