1a5c83c1b35dc63e05286fab51088d8b939a4e8a
Wolfie873 <jorge.ortiz20@outlook.com>
2024-04-27 10:06:59 -0400

    main

--- Python Webpage/main.py
+++ Python Webpage/main.py
@@ -1,11 +1,11 @@
 import pymongo
-import datetime
+import asyncio
+import tornado
 import tornado.ioloop
 import tornado.web
 
 
 from pymongo import MongoClient
-from pyscript import document
 
 client = MongoClient("mongodb://localhost:27017/")
 
@@ -13,10 +13,16 @@ db = client.bookstore
 books_collection = db.books
 
 
-def search_book(event):
-        book_title = document.querySelector("#english")
-        output_text = book_title.value
-        output_div = document.querySelector("#output")
-        output_div.innerText = output_text
-        
-        
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+async def main():
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    application.listen(8888)
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
